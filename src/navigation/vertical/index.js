500f7fb4bdcc28d863144f75539a1fd04c27a26d
vishwas1 <vishwasanand.cse@gmail.com>
2022-09-07 20:04:35 +0530

    updated design

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -13,15 +13,15 @@ function processMenu() {
     blockchains.push(menu)
   })
 
-  if (blockchains.length > 1) {
-    chainMenus.push({ header: 'ecosystem' })
-    chainMenus.push({
-      title: 'blockchains',
-      children: blockchains,
-      tag: `${blockchains.length}`,
-      icon: 'https://ping.pub/logo.svg',
-    })
-  }
+  // if (blockchains.length > 1) {
+  //   chainMenus.push({ header: 'ecosystem' })
+  //   chainMenus.push({
+  //     title: 'blockchains',
+  //     children: blockchains,
+  //     tag: `${blockchains.length}`,
+  //     icon: 'https://ping.pub/logo.svg',
+  //   })
+  // }
   chainMenus.push({ header: 'LINKS' })
   if (isTestnet()) {
     chainMenus.push({
@@ -30,30 +30,30 @@ function processMenu() {
       icon: 'ChromeIcon',
     })
   } else {
-    chainMenus.push({
-      title: 'Testnet Explorer',
-      href: 'http://testnet.ping.pub',
-      icon: 'LifeBuoyIcon',
-    })
+    // chainMenus.push({
+    //   title: 'Testnet Explorer',
+    //   href: 'http://testnet.ping.pub',
+    //   icon: 'LifeBuoyIcon',
+    // })
   }
   chainMenus.push({
     title: 'Github',
-    href: 'https://github.com/ping-pub/explorer',
+    href: 'https://github.com/hypersign-protocol/hid-node',
     icon: 'GithubIcon',
   })
   chainMenus.push({
     title: 'Discord',
-    href: 'https://discord.gg/CmjYVSr6GW',
+    href: 'https://discord.gg/ZKVJuVq7',
     icon: 'EyeIcon',
   })
   chainMenus.push({
     title: 'Twitter',
-    href: 'https://twitter.com/ping_pub',
+    href: 'https://twitter.com/hypersignchain/',
     icon: 'TwitterIcon',
   })
   chainMenus.push({
     title: 'Telegram',
-    href: 'https://t.me/pingpub',
+    href: 'https://t.me/hypersignchain',
     icon: 'SendIcon',
   })
 
